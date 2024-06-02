556f4bcbe7499c925ab04c577a25da9c1aa0f704
gunjury <gunjury@gmail.com>
2024-06-02 11:12:09 -0700

    Get data from api for name

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 function App() {
+  let [data, setData] = useState('');
+  let [name, setName] = useState('');
+
+  function getData(){
+  fetch(`https://greetapi2-yg-as.azurewebsites.net/greet/hi?name=${name}`)
+  .then(res => res.json())
+  .then(d => setData(d));
+  }
+  
   const value = 'World';
+  
   return <>
     <div>Hello {value}</div>
-  <button onClick={e => alert('foo')}>Call api</button>
+    <label for="name">Name</label>
+    <input name="Name" onChange={e => setName(e.target.value)}>
+    <button onClick={getData}>Call api</button>
+    
+    <div> {data} </div>
     </>;
 }
 
